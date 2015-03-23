54672c1d1722c5299bfdd7d50573a7f49c6aa00b
aglab2 <aglab3@gmail.com>
2015-03-23 23:28:45 +0300

    Допилен триггер на турнир

--- Database_scripts/Triggers/T1.sql
+++ Database_scripts/Triggers/T1.sql
@@ -11,9 +11,50 @@ AS
 	if (@att_id IS NULL)
 		raiserror('No such historical entry!', 18, -1);
 
-	INSERT INTO History(instance_id, attribute_id, modification_date,gameround_id,previous_value) 
-		SELECT deleted.id, @att_id, GETDATE(), 1, deleted.name
+	INSERT INTO History(instance_id, attribute_id, modification_date, previous_value) 
+		SELECT deleted.id, @att_id, GETDATE(), deleted.name
 		FROM deleted, inserted
 		WHERE deleted.id = inserted.id
 		AND deleted.name != inserted.name
 GO
+
+GO
+IF OBJECT_ID ('T_Tournament', 'TR') IS NOT NULL
+   DROP TRIGGER T_Tournament;
+GO
+CREATE TRIGGER T_Tournament
+	ON Tournament
+	AFTER UPDATE
+AS
+	DECLARE @att_id INT;
+	SET @att_id = (SELECT id FROM EntityAttributeDict WHERE name='Tournament.name')
+	if (@att_id IS NULL)
+		raiserror('No such historical entry! (Tournament.name)', 18, -1);
+
+	INSERT INTO History(instance_id, attribute_id, modification_date, previous_value) 
+		SELECT deleted.id, @att_id, GETDATE(), deleted.name
+		FROM deleted, inserted
+		WHERE deleted.id = inserted.id
+		AND deleted.name != inserted.name
+
+	SET @att_id = (SELECT id FROM EntityAttributeDict WHERE name='Tournament.address_id')
+	if (@att_id IS NULL)
+		raiserror('No such historical entry! (Tournament.address_id)', 18, -1);
+
+	INSERT INTO History(instance_id, attribute_id, modification_date, previous_value) 
+		SELECT deleted.id, @att_id, GETDATE(), deleted.address_id
+		FROM deleted, inserted
+		WHERE deleted.id = inserted.id
+		AND deleted.address_id != inserted.address_id
+
+	SET @att_id = (SELECT id FROM EntityAttributeDict WHERE name='Tournament.password')
+	if (@att_id IS NULL)
+		raiserror('No such historical entry! (Tournament.password)', 18, -1);
+
+	INSERT INTO History(instance_id, attribute_id, modification_date, previous_value) 
+		SELECT deleted.id, @att_id, GETDATE(), deleted.password
+		FROM deleted, inserted
+		WHERE deleted.id = inserted.id
+		AND deleted.password != inserted.password
+GO
+
