fe85139605e59127e66387e346584afa38940c82
Kirill <kshcherbatov@gmail.com>
2015-04-06 00:49:38 +0300

    Написан прототип тришера для Coordinator

--- Database_scripts/CoordinatorTest.sql
+++ Database_scripts/CoordinatorTest.sql
@@ -1,39 +1,96 @@
-CREATE VIEW Coordinator_Competantion_Tournament
-AS SELECT * 
-    FROM Tournament 
-    WHERE name = 'ШРеК' AND address_id = 1
-    WITH CHECK OPTION;
+CREATE TABLE Users(
+	id integer IDENTITY(1,1) NOT NULL,
+	name      nvarchar(255) NOT NULL,
+	userrole  nvarchar(25) NOT NULL,
+	season_id integer NOT NULL,
+	address_id integer NOT NULL,
+	tournament_name nvarchar(255) NOT NULL
+);
 GO
 
-DROP USER Coordinator007
-DROP LOGIN Coordinator007
-DROP ROLE Coordinator
-	
-CREATE ROLE Coordinator;	
+CREATE ROLE Coordinator
 
-CREATE LOGIN Coordinator007 
-	WITH PASSWORD = '007',
-		DEFAULT_DATABASE = KBT_IS;
-
-CREATE USER Coordinator007  FOR LOGIN Coordinator007;
-EXEC sp_addrolemember @rolename = 'Coordinator', @membername = 'Coordinator007';
-
-GRANT UPDATE, ON Coordinator_Competantion_Tournament TO Coordinator007; 
-
-GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE ON Player TO Coordinator
-GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE ON Team TO Coordinator
-GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE ON GameRound TO Coordinator
-GRANT EXECUTE ON OBJECT::dbo.addTeam2Tournament TO Coordinator
+GRANT SELECT ON OBJECT::dbo.Season TO Coordinator
+GRANT SELECT ON OBJECT::dbo.PlayerTeamGameround TO Coordinator
+GRANT SELECT ON OBJECT::dbo.AgeCategory TO Coordinator
+GRANT SELECT ON OBJECT::dbo.AddressType TO Coordinator
+GRANT SELECT ON OBJECT::dbo.PlayerSeason TO Coordinator
+GRANT SELECT ON OBJECT::dbo.History TO Coordinator
+GRANT SELECT ON OBJECT::dbo.EntityAttributeDict TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Tournament TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.TeamTournament TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.GameRound TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Team TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Player TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Answer TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Question TO Coordinator
+GRANT SELECT, INSERT, UPDATE, DELETE ON OBJECT::dbo.Address TO Coordinator
 go
 
+CREATE PROCEDURE addCoordinatorUser(
+	@name nvarchar(255),
+	@password nvarchar(255),
+	@tournament_name nvarchar(255),
+	@season_id integer,
+	@address_name nvarchar(255))
+AS
+	EXEC sp_addlogin @name, @password;
+	EXEC sp_adduser @name, @name, 'Coordinator'
+	DECLARE @address_id INT;
+	SET @address_id = (SELECT id FROM Address WHERE Address.name = @address_name)
+	INSERT INTO Users (name, userrole, season_id, address_id, tournament_name)
+	VALUES (@name, 'Coordinator', @season_id, @address_id, @tournament_name)
+GO
+
+exec addCoordinatorUser @name = 'Vasya2', @password='123', @season_id = 1, @address_name = 'Москва', @tournament_name = 'ШРеК';
+
+IF OBJECT_ID ('Concret_GameRound_Update_Coordinator', 'TR') IS NOT NULL
+   DROP TRIGGER Concret_GameRound_Update_Coordinator;
+GO
+
+CREATE TRIGGER Concret_GameRound_Update_Coordinator
+	ON GameRound
+	INSTEAD OF UPDATE
+AS	
+	DECLARE @user_name NVARCHAR(255);
+	SET @user_name = (SELECT CURRENT_USER);
+
+	DECLARE @userrole  nvarchar(25);
+	DECLARE @tournament_name  nvarchar(255);
+	DECLARE @season_id integer;
+	DECLARE @address_id integer;
+
+	SET @userrole = (SELECT season_id FROM Users WHERE Users.name = @user_name);
+	SET @season_id  = (SELECT season_id FROM Users WHERE Users.name = @user_name);
+	SET @address_id = (SELECT address_id FROM Users WHERE Users.name = @user_name);
+	SET @tournament_name = (SELECT tournament_name FROM Users WHERE Users.name = @user_name);
+
+	IF (@userrole != 'Coordinator') RETURN;
+	
+	DECLARE @tournament_id integer;
+	SET @tournament_id = (SELECT id FROM Tournament WHERE Tournament.name = @tournament_name AND Tournament.address_id = @address_id AND Tournament.season_id = @season_id AND Tournament.address_id = @address_id);
+
+	INSERT INTO GameRound
+	VALUES (SELECT * FROM inserted WHERE inserted.tournament_id = @tournament_id);
+GO
+
+DROP PROCEDURE addCoordinatorUser;
+DROP LOGIN Vasya
+DROP USER Vasya
+DROP TABLE Users;
+DROP ROLE Coordinator
+	
+
+
+
+
+
+go
 
 -- Составляем отчет обо всех пользователях базы данных, утративших связь с именем входа
-EXECUTE sp_change_users_login @Action='Report';
+--EXECUTE sp_change_users_login @Action='Report';
 
--- Проверяем, принадлежит ли текущий пользователь к роли db_owner
-SELECT IS_MEMBER ("db_owner");
-
--- Права, предоставленные...
+/* Права, предоставленные...
 SELECT dp.class_desc, s.name AS 'Schema', o.name AS 'Object', dp.permission_name, 
        dp.state_desc, prin.[name] AS 'User'
 FROM sys.database_permissions dp
@@ -52,4 +109,4 @@ FROM sys.database_permissions dp
     ON dp.grantee_principal_id = prin.principal_id
   JOIN sys.schemas s
     ON dp.major_id = s.schema_id
-WHERE dp.class_desc = 'SCHEMA'
+WHERE dp.class_desc = 'SCHEMA'*/
