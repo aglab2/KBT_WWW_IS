9a1d2bd671d81f3ad3f8bbe287cc9d717b07fd4e
Alexey Neganov <neganovalexey@gmail.com>
2015-04-06 11:53:26 +0300

    ААА, вьююшчки

--- Database_scripts/ViewTest.sql
+++ Database_scripts/ViewTest.sql
@@ -4,11 +4,12 @@ IF OBJECT_ID('GameRound_Coordinator', 'V') IS NOT NULL
 GO
 CREATE VIEW GameRound_Coordinator AS
 	SELECT GameRound.tournament_id, GameRound.gamenumber, GameRound.id
-		FROM GameRound, Users
+		FROM GameRound, Users, Tournament
 		WHERE Users.name = CURRENT_USER
 		AND IS_MEMBER('Сoordinator') = 1
 		AND GameRound.tournament_id = Tournament.id
 		AND Tournament.address_id = Users.address_id
+WITH CHECK OPTION;
 		
 GO
 IF OBJECT_ID('GameRound_Organizer', 'V') IS NOT NULL
@@ -20,6 +21,7 @@ CREATE VIEW GameRound_Organizer AS
 		WHERE Users.name = CURRENT_USER
 		AND IS_MEMBER('Organizer') = 1
 		AND GameRound.tournament_id = Users.tournament_id
+WITH CHECK OPTION;
 
 GO
 IF OBJECT_ID('Tournament_Coordinator', 'V') IS NOT NULL
@@ -31,6 +33,7 @@ CREATE VIEW Tournament_Coordinator AS
 		WHERE Users.name = CURRENT_USER
 		AND IS_MEMBER('Сoordinator') = 1
 		AND Tournament.address_id = Users.address_id
+WITH CHECK OPTION;
 
 GO
 IF OBJECT_ID('Tournament_Organizer', 'V') IS NOT NULL
@@ -42,3 +45,31 @@ CREATE VIEW Tournament_Organizer AS
 		WHERE Users.name = CURRENT_USER
 		AND IS_MEMBER('Organizer') = 1
 		AND Tournament.tournament_id = Users.tournament_id
+WITH CHECK OPTION;
+
+GO
+IF OBJECT_ID ('TeamTournament_Coordinator', 'V') IS NOT NULL
+	DROP VIEW TeamTournament_Coordinator;
+GO
+CREATE VIEW TeamTournament_Coordinator AS
+	SELECT TeamTournament.tournament_id, TeamTournament.team_id, TeamTournament.age_category_id, TeamTournament.regcard_number
+		FROM TeamTournament, Users, Tournament
+		WHERE Users.name = CURRENT_USER
+		AND IS_MEMBER('Coordinator') = 1
+		AND TeamTournament.tournament_id = Tournament.id
+		AND Tournament.address_id = Users.address_id
+WITH CHECK OPTION;
+GO
+
+GO
+IF OBJECT_ID ('TeamTournament_Organizer', 'V') IS NOT NULL
+	DROP VIEW TeamTournament_Organizer;
+GO
+CREATE VIEW TeamTournament_Organizer AS
+	SELECT TeamTournament.tournament_id, TeamTournament.team_id, TeamTournament.age_category_id, TeamTournament.regcard_number
+		FROM TeamTournament, Users
+		WHERE Users.name = CURRENT_USER
+		AND IS_MEMBER('Organizer') = 1
+		AND TeamTournament.tournament_id = Users.tournament_id
+WITH CHECK OPTION;
+GO
