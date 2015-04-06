29bf7c2e42c524f4b1b2bbea389b2d23b4a99995
Alexey Neganov <neganovalexey@gmail.com>
2015-04-06 11:08:31 +0300

    Проверочка

--- Database_scripts/ViewTest.sql
+++ Database_scripts/ViewTest.sql
@@ -1,4 +1,6 @@
-DROP VIEW GameRound_User
+GO
+IF OBJECT_ID('GameRound_User', 'V') IS NOT NULL
+	DROP VIEW GameRound_User
 GO
 CREATE VIEW GameRound_User AS
 	SELECT GameRound.tournament_id, GameRound.gamenumber, GameRound.id
